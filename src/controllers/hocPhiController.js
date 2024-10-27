89fef2e4d40d40cd38ff838c6cbf37322c6ef069
vuthanhdat2k3 <vuthanhdat19052k3@gmail.com>
2024-10-27 14:57:41 +0700

    create api xemHocPhi

--- src/controllers/hocPhiController.js
+++ src/controllers/hocPhiController.js
@@ -1,10 +1,10 @@
-const {runPuppeteer} = require('../services/hocPhiService'); // Đảm bảo bạn đã xuất hàm crawl từ file crawlXemHocPhi.js
+const {crawlHocPhi} = require('../services/hocPhiService'); // Đảm bảo bạn đã xuất hàm crawl từ file crawlXemHocPhi.js
 
 // Controller để crawl dữ liệu học phí
 const hocPhiController = {
   async crawlData(req, res) {
     try {
-      const browser = await runPuppeteer(); // Chạy hàm crawlXemHocPhi
+      const browser = await crawlHocPhi(); // Chạy hàm crawlXemHocPhi
       console.log("Crawl dữ liệu thành công!");
       res.status(200).json({ message: "Crawl dữ liệu thành công!" });
     } catch (error) {
