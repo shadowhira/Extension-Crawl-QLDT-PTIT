9cb9a110c3c8b4290c01159b1389ca566331ec6c
shadowhira <thanhoc890@gmail.com>
2024-10-27 09:40:20 +0700

    add backend

---
+++ src/app.js
@@ -0,0 +1,14 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Route đơn giản
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+// Bắt đầu server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
